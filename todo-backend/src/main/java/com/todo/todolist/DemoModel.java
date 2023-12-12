1a3c54f02eef50fd20536ca5bdc2f4dc1b27055b
hocube <hocube159@gmail.com>
2023-12-12 23:19:51 +0900

    1212 롬복 테스팅 완료

---
+++ todo-backend/src/main/java/com/todo/todolist/DemoModel.java
@@ -0,0 +1,13 @@
+package com.todo.todolist;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class DemoModel {
+
+	@NonNull
+	private String id;
+}
